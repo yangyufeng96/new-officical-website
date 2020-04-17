d506ce1dd3568d1dfaccb0bf86ecfcb72ab429ba
陈铭 <18856619010@163.com>
2020-04-17 14:59:39 +0800

    app banner修改

--- js/rollList.js
+++ js/rollList.js
@@ -1,14 +1,35 @@
 $('.btnBox').mouseenter(function () {
   $(this).children().attr("style", "display:block;");
 })
-// $('.dropdown-menu').mouseenter(function () {
-//   $(this).attr("style", "display:block;");
-// })
-$('.mask').click(function () {
-  
-})
 $('.btnBox').mouseleave(function () {
   $(this).children().eq(1).attr("style", "display:none;");
 })
 
-// 划入导航
+window.onload = function () {
+  var arr = $('.dropdown').find('a')
+  var selId = ''
+  for (var index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    const ele = element.href.split('/');
+    const newele = '/' + ele[ele.length - 1];
+    if (newele == window.location.pathname) {
+      selId = index;
+    }
+  }
+  $('.dropdown').find('a').removeClass('navActive');
+  $('.dropdown').find('a').eq(selId).addClass('navActive');
+
+  var arrf = $('.footer-down').find('a')
+  var selIdf = ''
+  for (var indexf = 0; indexf < arrf.length; indexf++) {
+    const elementf = arrf[indexf];
+    const elef = elementf.href.split('/');
+    const newelef = '/' + elef[elef.length - 1];
+    if (newelef == window.location.pathname) {
+      selIdf = indexf;
+    }
+  }
+  $('.footer-down').find('a').find('li').removeClass('li-active');
+  $('.footer-down').find('a').eq(selIdf).children('li').addClass('li-active');
+}
+
